c5b0280f287f801b89f348faaadd71d482200a6f
TheMessifan10 <89708451+TheMessifan10@users.noreply.github.com>
2021-09-14 17:31:01 -0400

    Create keys.py

---
+++ config/keys.py
@@ -0,0 +1,11 @@
+import giphy_client
+WOLFRAM_API_KEY = ''
+api_key = ''
+token = ''
+api_key_giphy = ""
+api_instance = giphy_client.DefaultApi()
+client_id =  ""
+api_key_weather = ""
+base_url = ""
+
+#i deleted some api's so some of them are useless
